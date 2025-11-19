1b7c921aef845d7bdd5fc918751c44a508731168
berat-berber <berat0berber@gmail.com>
2025-11-19 20:16:41 +0300

    Set up database connection and implement asp.net identity user

---
+++ Backend/Models/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Backend;
+
+public class User : IdentityUser
+{
+
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,9 @@
+using Backend;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +12,37 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Get connection string from .net user secrets
+var ConnectionString = builder.Configuration.GetConnectionString("ConnectionString");
+
+// Set up PostgreSQL database connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(ConnectionString));
+
+// Add identity service
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+}
+
+// Create roles
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    if (!await roleManager.RoleExistsAsync("Admin"))
+        await roleManager.CreateAsync(new IdentityRole("Admin"));
+
+    if (!await roleManager.RoleExistsAsync("User"))
+        await roleManager.CreateAsync(new IdentityRole("User"));
 }
 
 app.UseHttpsRedirection();
