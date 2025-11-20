4ef1e1955c4d1b18624cc23986d61e13d36852da
berat-berber <berat0berber@gmail.com>
2025-11-20 16:02:43 +0300

    Add user register endpoint

---
+++ Backend/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+using Backend;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        
+        private readonly UserManager<User> _userManager;
+
+        public AuthController(UserManager<User> userManager) => _userManager = userManager;
+
+        [HttpPost]
+        public async Task<ActionResult> Register([FromBody] RegisterRequest request)
+        {
+            var response = await _userManager.FindByEmailAsync(request.Email);
+
+            if (response is not null) return BadRequest("User Exists");
+
+            var user = new User() {Email = request.Email};
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+            if (!result.Succeeded) return BadRequest(result);
+
+            return Ok();
+        }
+
+    }
+}
