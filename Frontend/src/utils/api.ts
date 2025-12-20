6bcdfc527cc50b405a3195409580c23617caf294
berat-berber <berat0berber@gmail.com>
2025-12-20 12:13:53 +0300

    Update api fetch functions

--- Frontend/src/utils/api.ts
+++ Frontend/src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5134/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function fetchWithAuth(endpoint: string, options: RequestInit = {}) {
   const token = localStorage.getItem('token');
